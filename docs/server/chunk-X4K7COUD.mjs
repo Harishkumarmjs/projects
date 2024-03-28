8142f705bd700e4d984d265fba92402ec3eb2dbe
ashwinnexconz <ashwin@nexconz.com>
2024-03-28 18:45:00 +0530

    changes

--- docs/server/chunk-X4K7COUD.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{V as c,d as r,g as i,ja as s}from"./chunk-LRV7JECX.mjs";import"./chunk-VVCT4QZE.mjs";var a=[],m=(()=>{let t=class t{};t.\u0275fac=function(e){return new(e||t)},t.\u0275mod=r({type:t}),t.\u0275inj=i({imports:[s.forChild(a),s]});let o=t;return o})();var h=(()=>{let t=class t{};t.\u0275fac=function(e){return new(e||t)},t.\u0275mod=r({type:t}),t.\u0275inj=i({imports:[c,m]});let o=t;return o})();export{h as AdminModule};
